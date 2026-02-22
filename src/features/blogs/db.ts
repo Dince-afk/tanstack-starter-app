155b1756e19dd860320b6edfc0a53e4e2ae2128a
Dino Curic <dino.c@me.com>
2026-02-22 22:50:12 +0100

    fix: refactor getBlogs to use createServerFn and clean up commented code

--- src/features/blogs/db.ts
+++ src/features/blogs/db.ts
@@ -1,16 +1,16 @@
-// import { createServerFn } from "@tanstack/react-start";
+import { createServerFn } from "@tanstack/react-start";
 import postgres from "postgres";
 
 const sql = postgres(process.env.DATABASE_URL!);
 
-// export const getBlogs = createServerFn({ method: "GET" }).handler(async () => {
-//   console.log("Fetching blogs");
-//   const data = await sql`SELECT * FROM blogs`;
-//   return data;
-// });
-
-export async function getBlogs() {
+export const getBlogs = createServerFn({ method: "GET" }).handler(async () => {
   console.log("Fetching blogs");
   const data = await sql`SELECT * FROM blogs`;
   return data;
-}
+});
+
+// export async function getBlogs() {
+//   console.log("Fetching blogs");
+//   const data = await sql`SELECT * FROM blogs`;
+//   return data;
+// }
