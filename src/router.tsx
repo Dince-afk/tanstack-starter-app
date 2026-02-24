1bd51e5198574c04f540f7e42373e6c305139cc5
Dino Curic <dino.c@me.com>
2026-02-24 23:54:45 +0100

    fix: update defaultPreloadStaleTime to 10 seconds for improved performance

--- src/router.tsx
+++ src/router.tsx
@@ -7,7 +7,7 @@ export function getRouter() {
 
     scrollRestoration: true,
     defaultPreload: "intent",
-    defaultPreloadStaleTime: 0,
+    defaultPreloadStaleTime: 10_000,
     defaultNotFoundComponent: () => <div>Not found</div>,
   });
 
